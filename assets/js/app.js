69e5a791cb9ddb0aa72383af7d63a7fe174dd865
GioLorenzo <gio.lorenzo.lui@gmail.com>
2021-01-16 14:21:30 -0500

    Added functions and dynamic

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,52 +1,83 @@
-//Assign DOM elements
-
+// Assign DOM elements
 const time = document.getElementById('time'),
   greeting = document.getElementById('greeting'),
   name = document.getElementById('name'),
   focus = document.getElementById('focus'),
   amOrPm = document.getElementById('am-pm');
 
-//Show Time
 
+// Show Time
 function showTime() {
   let today = new Date(),
     hour = today.getHours(),
     min = today.getMinutes(),
     sec = today.getSeconds();
   
-  //Set AM or PM
+  // Set AM or PM
   const amPm = hour >= 12 ? 'PM' : 'AM';
 
   // 12hr Format
   hour = hour % 12 || 12;
 
-  //Output Time
+  // Output Time
   time.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(sec)}`;
   amOrPm.innerHTML = `${amPm}`;
 
-  //Refresh Every Second
+  // Refresh Every Second
   setTimeout(showTime, 1000);
 }
 
-//Add zero in front of single digit values
+
+// Add zero in front of single digit values
 const addZero = n => ((parseInt(n, 10) < 10) ? '0' : '') + n;
 
-//Set Background and Greeting
+
+// Set Background and Greeting
 const setBg = () => {
   let today = new Date(),
     hour = today.getHours();
-  
-  if ( hour > 5 && hour < 12) {
+  if (hour < 12) {
     //Morning
-    document.body.style.backgroundImage = "url('')";
-  } else if (hour > 11 && hour < 16) {
-    //Afternoon
-    document.body.style.backgroundImage = "url('')";
+    document.body.style.backgroundImage = "url('../assets/img/Morning.jpg')";
+    greeting.textContent = 'Good Morning';
+  } else if (hour < 18) {
+    // Afternoon
+    document.body.style.backgroundImage = "url('../assets/img/Afternoon.jpg')";
+    greeting.textContent = 'Good Afternoon';
+    document.body.style.color = 'white';
   } else {
-    //Evening
-    document.body.style.backgroundImage = "url('')";
+    // Evening
+    document.body.style.backgroundImage = "url('../assets/img/night.jpg')";
+    greeting.textContent = 'Good Evening';
+    document.body.style.color = 'white';
   }
 }
 
-//run
+// Get Name
+function getName() {
+  if (localStorage.getItem('name') === null) {
+    name.textContent = '[Your Name]'
+  } else {
+    name.textContent = localStorage.getItem('name');
+  }
+}
+
+
+
+// Get Focus
+function getFocus() {
+  if (localStorage.getItem('name') === null) {
+    focus.textContent = '[Enter Focus Here]'
+  } else {
+    focus.textContent = localStorage.getItem('focus');
+  }
+}
+
+
+
+
+// run
+getName();
+getFocus();
+setBg();
 showTime();
