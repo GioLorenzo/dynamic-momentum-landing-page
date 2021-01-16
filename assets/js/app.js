d1744b0a7d02d3fcb1e9a82a51c441e2fd3b27b1
GioLorenzo <gio.lorenzo.lui@gmail.com>
2021-01-16 12:29:22 -0500

    added clock widget in javascript

---
+++ assets/js/app.js
@@ -0,0 +1,30 @@
+//Assign DOM elements
+
+const time = document.getElementById('time'),
+  greeting = document.getElementById('greeting'),
+  name = document.getElementById('name'),
+  focus = document.getElementById('focus'),
+  amOrPm = document.getElementById('am-pm');
+
+//Show Time
+
+function showTime() {
+  let today = new Date(),
+    hour = today.getHours(),
+    min = today.getMinutes(),
+    sec = today.getSeconds();
+  
+  //Set AM or PM
+  const amPm = hour >= 12 ? 'PM' : 'AM';
+
+  // 12hr Format
+  hour = hour % 12 || 12;
+
+  //Output Time
+  time.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(sec)}`;
+  amOrPm.innerHTML = `${amPm}`;
+
+  //Refresh Every Second
+  setTimeout(showTime, 1000);
+}
+
