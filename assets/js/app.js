89c99cca112a84f16f73830415d193fac85d4aca
GioLorenzo <gio.lorenzo.lui@gmail.com>
2021-01-16 14:21:44 -0500

    added set name and set focus

--- assets/js/app.js
+++ assets/js/app.js
@@ -62,7 +62,17 @@ function getName() {
   }
 }
 
-
+// Set Name
+function setName(e) {
+  if (e.type === 'keypress') {
+    if (e.which == 13 || e.keyCode == 13) {
+      localStorage.setItem('name', e.target.innerText);
+      name.blur();
+    }
+  } else {
+    localStorage.setItem('name', e.target.innerText);
+  }
+}
 
 // Get Focus
 function getFocus() {
@@ -73,8 +83,26 @@ function getFocus() {
   }
 }
 
+// Set Focus
+function setFocus(e) {
+  if (e.type === 'keypress') {
+    if (e.which == 13 || e.keyCode == 13) {
+      localStorage.setItem('focus', e.target.innerText);
+      focus.blur();
+    }
+  } else {
+    localStorage.setItem('focus', e.target.innerText);
+  }
+}
 
 
+name.addEventListener('keypress', setName);
+name.addEventListener('blur', setName);
+  
+
+focus.addEventListener('keypress', setFocus);
+focus.addEventListener('blur', setFocus);
+  
 
 // run
 getName();
