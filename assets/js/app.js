2696fb7e36a6807b14ae310170972e681cf0b87e
GioLorenzo <gio.lorenzo.lui@gmail.com>
2021-01-16 12:30:00 -0500

    setting background images

--- assets/js/app.js
+++ assets/js/app.js
@@ -31,3 +31,22 @@ function showTime() {
 //Add zero in front of single digit values
 const addZero = n => ((parseInt(n, 10) < 10) ? '0' : '') + n;
 
+//Set Background and Greeting
+const setBg = () => {
+  let today = new Date(),
+    hour = today.getHours();
+  
+  if ( hour > 5 && hour < 12) {
+    //Morning
+    document.body.style.backgroundImage = "url('')";
+  } else if (hour > 11 && hour < 16) {
+    //Afternoon
+    document.body.style.backgroundImage = "url('')";
+  } else {
+    //Evening
+    document.body.style.backgroundImage = "url('')";
+  }
+}
+
+//run
+showTime();
